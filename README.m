5ce9d50347b558bafea92f10d402fd4f359c51eb
mosigisidesreeh-cloud <mosigisidesreeh@gmail.com>
2025-10-02 13:09:49 +0300

    Create README.m

---
+++ README.m
@@ -0,0 +1,31 @@
+📚 Online Library Management System (C++)
+
+This project is a basic Online Library Management System implemented in C++ using Object-Oriented Programming (OOP) principles. It allows managing books and users, with functionality to add/remove books, search books, and enable users to borrow and return books.
+
+🛠 Features
+
+Add and remove books from the library
+
+Search books by:
+
+Title
+
+ISBN
+
+Add users to the library
+
+Users can:
+
+Borrow available books
+
+Return borrowed books
+
+View list of borrowed books
+
+Displays all books with availability status
+
+Includes basic test cases in main()
+
+🧱 Classes Used
+Class	Responsibility
+Book	
